423e72e3019612f08fa86b806d1292e28594937c
Srg90 <usikov90@mail.ru>
2022-03-17 23:11:03 +0300

    levelRoof + NewExtrusionRoof

--- CreateModel.cs
+++ CreateModel.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.ApplicationServices;
+
 
 namespace RevitAPIUI10CreateModel
 {
@@ -40,7 +42,21 @@ namespace RevitAPIUI10CreateModel
                 }
                 ts2.Commit();
             }
-            
+
+            Transaction ts3 = new Transaction(doc, "Добавление уровня крыши");
+            {
+                ts3.Start();
+                AddRoof(commandData, levels.Where(x => x.Name.Equals("Уровень 2")).FirstOrDefault(), walls);
+
+                double elevValue = 4125;
+                double elevation = UnitUtils.ConvertToInternalUnits(elevValue, UnitTypeId.Millimeters);
+                Level levelRoof = Level.Create(doc, elevation);
+                levels.Add(levelRoof);
+
+                AddRoofExtrusion(commandData, levelRoof, walls, 32.8, 16.4);
+                ts3.Commit();
+            }
+
             return Result.Succeeded;
         }
 
@@ -175,6 +191,94 @@ namespace RevitAPIUI10CreateModel
                                                        .ToList();
 
             return walllist;
+        }
+
+        private static void AddRoof(ExternalCommandData commandData, Level level2, List<Wall> walls)
+        {
+            var doc = commandData.Application.ActiveUIDocument.Document;
+            Application application = doc.Application;
+
+            List<Level> levels = GetLevels(commandData);
+            
+            RoofType roofType = new FilteredElementCollector(doc)
+                .OfClass(typeof(RoofType))
+                .OfType<RoofType>()
+                .Where(x => x.Name.Equals("Типовой - 125мм"))
+                .Where(x => x.FamilyName.Equals("Базовая крыша"))
+                .FirstOrDefault();
+
+            double wallWidth = walls[0].Width;
+            double dt = wallWidth / 2;
+
+            List<XYZ> points = new List<XYZ>();
+            points.Add(new XYZ(-dt, -dt, 0));
+            points.Add(new XYZ(dt, -dt, 0));
+            points.Add(new XYZ(dt, dt, 0));
+            points.Add(new XYZ(-dt, dt, 0));
+            points.Add(new XYZ(-dt, -dt, 0));
+
+            CurveArray footprint = application.Create.NewCurveArray();
+
+            for (int i = 0; i < 4; i++)
+            {
+                LocationCurve curve = walls[i].Location as LocationCurve;
+                XYZ p1 = curve.Curve.GetEndPoint(0);
+                XYZ p2 = curve.Curve.GetEndPoint(1);
+                Line line = Line.CreateBound(p1 + points[i], p2 + points[i + 1]);
+                footprint.Append(line);
+            }
+
+            ModelCurveArray footPrintToModelCurveMapping = new ModelCurveArray();
+            FootPrintRoof footprintRoof = doc.Create.NewFootPrintRoof(footprint, level2, roofType, out footPrintToModelCurveMapping);
+
+            //ModelCurveArrayIterator iterator = footPrintToModelCurveMapping.ForwardIterator();
+            //iterator.Reset();
+            //while (iterator.MoveNext())
+            //{
+            //    ModelCurve modelCurve = iterator.Current as ModelCurve;
+            //    footprintRoof.set_DefinesSlope(modelCurve, true);
+            //    footprintRoof.set_SlopeAngle(modelCurve, 0.5);
+            //}
+
+            //foreach (ModelCurve m in footPrintToModelCurveMapping)
+            //{
+            //    footprintRoof.set_DefinesSlope(m, true);
+            //    footprintRoof.set_SlopeAngle(m, 0.5);
+            //}
+        }
+
+        private static void AddRoofExtrusion(ExternalCommandData commandData, Level level2, List<Wall> walls, double width, double depth)
+        {
+            var doc = commandData.Application.ActiveUIDocument.Document;
+            View v = doc.ActiveView;
+
+            RoofType roofType = new FilteredElementCollector(doc)
+                .OfClass(typeof(RoofType))
+                .OfType<RoofType>()
+                .Where(x => x.Name.Equals("Типовой - 125мм"))
+                .Where(x => x.FamilyName.Equals("Базовая крыша"))
+                .FirstOrDefault();
+
+            double wallWidth = walls[0].Width;
+            double dt = wallWidth / 2;
+
+            double extrusionStart = -width / 2 - dt;
+            double extrusionEnd = width / 2 + dt;
+
+            double curveStart = -depth / 2 - dt;
+            double curveEnd = +depth / 2 + dt;
+
+            //double elevValue = 4125;
+            //double elevation = UnitUtils.ConvertToInternalUnits(elevValue, UnitTypeId.Millimeters);
+            //Level levelRoof = Level.Create(doc, elevation);
+
+            CurveArray curveArray = new CurveArray();
+            curveArray.Append(Line.CreateBound(new XYZ(0, curveStart, level2.Elevation), new XYZ(0, 0, level2.Elevation + 10)));
+            curveArray.Append(Line.CreateBound(new XYZ(0, 0, level2.Elevation + 10), new XYZ(0, curveEnd, level2.Elevation)));
+
+            ReferencePlane plane = doc.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(0, 0, 20), new XYZ(0, 20, 0), v);
+            ExtrusionRoof extrusionRoof = doc.Create.NewExtrusionRoof(curveArray, plane, level2, roofType, extrusionStart, extrusionEnd);
+            extrusionRoof.EaveCuts = EaveCutterType.TwoCutSquare;
         }
     }
 }
